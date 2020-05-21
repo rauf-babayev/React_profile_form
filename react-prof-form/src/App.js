16b129edb32ff56f64172e74d9466d81ce3e4270
rauf-babayev <rauf.rb.business@gmail.com>
2020-05-22 02:15:49 +0400

    RPF-1-first page layout done

--- react-prof-form/src/App.js
+++ react-prof-form/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
+import Input from './construct/input'
 
 function App() {
     return (
-        <div className='container'>
-            <div className='wrapper'>
-                <p className='heading'>Provide Your Credetionals</p>
-                <input className='name_input' placeholder='Enter your name...'/>
-                <input className='age_input' placeholder='Enter your age...'/>
-                <input className='email_input' placeholder='Enter your email...'/>
-                <input className='number_input'placeholder='Enter your number...'/>
-                <input className='submit_input' type='submit' value='SUBMIT'/>
+        <div className='first_page_container'>
+            <div className='welcome_container'>
+                <p className='welcome_heading'>Welcome To Your Our Page</p>
+                <input className='welcome_button' type='submit' value='Open'/>
             </div>
-        </div>);
-}
+        </div>
+);}
 
 export default App;
